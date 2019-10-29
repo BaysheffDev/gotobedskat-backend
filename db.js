eb7484a33f5870fa37e5eba01df65b8fed959537
BaysheffDev <42050015+BaysheffDev@users.noreply.github.com>
2019-10-29 23:19:14 +1030

    first backend commit

---
+++ db.js
@@ -0,0 +1,17 @@
+// ENVIRONMENT VARIABLES
+const dotenv = require('dotenv');
+dotenv.config();
+
+// POSTGRES DB CONNECTION
+const {Pool, Client} = require('pg');
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const PORT = process.env.PORT;
+const connectionString = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@localhost:${PORT}/gotobedskat`;
+const db = new Pool({
+	connectionString: connectionString,
+})
+
+module.exports = {
+  dbConnection: db
+}
---
+++ initializeTables.js
@@ -0,0 +1,46 @@
+// Require database connection
+const db = require('./db.js').dbConnection;
+
+const selectAll = {
+    text: 'SELECT *',
+    values: [],
+}
+
+const createUsersTable = {
+    text: `
+    CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(30) NOT NULL,
+        usercode VARCHAR(30) NOT NULL,
+        usercolor VARCHAR(30) NOT NULL,
+        partnerid INTEGER
+    )`,
+    values: [],
+}
+
+const createRecordsTable = {
+    text: `
+    CREATE TABLE records(
+        id SERIAL PRIMARY KEY,
+        userid INTEGER REFERENCES users(id),
+        date DATE NOT NULL,
+        bedtime VARCHAR(10) NOT NULL,
+        message TEXT
+    )`,
+    values: [],
+}
+
+tableQueries = [
+    createUsersTable,
+    createRecordsTable
+]
+
+async function runQueries(queries) {
+    for (let i = 0, len = queries.length; i < len; i++) {
+        await db.query(queries[i])
+        .then(res => console.log(res))
+        .catch(err => console.log("Error: ", err));
+    }
+}
+
+runQueries(tableQueries);
