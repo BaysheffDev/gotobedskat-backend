8573aa7cc9743f6ca38eaa41f1d94badef33aba3
Alex Saliu <alex.saliu@gmail.com>
2019-11-01 09:26:06 +1030

    building endpoints

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.post('/createuser', async (req, res) => {
     values: [username, usercode, usercolor]
   }
 
+ // Check username and usercode combo is unique
   try {
     const userCheck = await db.query(checkUsers);
     if (userCheck.rows.length > 0) {
@@ -52,6 +53,7 @@ app.post('/createuser', async (req, res) => {
     console.log("ERROR 1 /createuser: ", err);
   }
 
+  // Create the user if unique
   if (uniqueUser) {
     try {
       const data = await db.query(addUser);
@@ -65,11 +67,15 @@ app.post('/createuser', async (req, res) => {
 
 // TODO: POST Create user
 app.post('/loginuser', async (req, res) => {
+
   const { username, usercode } = req.body;
+
   const validateUser = {
     text: `SELECT * FROM users where username = $1 AND usercode = $2`,
     values: [username, usercode]
   }
+
+  // Check that the user exists, if yes return user info
   try {
     const data = await db.query(validateUser);
     if (data.rows.length > 0) {
@@ -86,44 +92,67 @@ app.post('/loginuser', async (req, res) => {
 
 // TODO: POST Sync with Partner
 app.post('/sync', async (req, res) => {
+
   const { id, partnername, partnercode } = req.body;
   const partnerId = "";
+
   const checkPartner = {
-    text: `SELECT * FROM users where username = $1 AND usercode = $2`,
+    text: `SELECT * FROM users WHERE username = $1 AND usercode = $2`,
     values: [partnername, partnercode]
   }
+  const syncUsers (idToFind, idToSync) => {
+    return {
+      text: `UPDATE users SET partnerid = $1 WHERE id = $2`,
+      values: [idToSync, idToFind]
+    }
+  }
 
+  // Check partner credentials exist
   try {
     const data = await db.query(checkPartner);
     partnerId = data.rows.length > 1 ? data.rows[0].id : false;
   }
   catch(err) {
-    console.log("ERROR /sync: ", err);
+    console.log("ERROR 1 /sync: ", err);
   }
-
+  // If partner credentials exist, sync user and partner
   if (partnerId) {
     try {
-
+      const syncPartnerToUser = db.query(syncUsers(id, partnerId));
+      const syncUserToPartner = db.query(syncUsers(partnerId, id));
+      res.json({success: true});
     }
     catch(err) {
-
+      console.log("ERROR 2 /sync: ", err);
     }
   }
 })
 
 // TODO: POST partner info and times
-app.post('/data', (req, res) => {
+app.post('/data', async (req, res) => {
+  const { userid, partnerid } = req.body;
+
+  const getRecords = (id) => {
+    return checkPartner = {
+      text: `SELECT * FROM records WHERE id = $1`,
+      values: [id]
+    }
+  }
+
+  try {
+    const userData = await db.query(getRecords(userid));
+    const partnerData = await db.query(getRecords(partnerid));
+    res.json({"userdata": userdata.rows, "partnerdata": partnerdata.rows});
+  }
+  catch(err) {
+    console.log("ERROR /data: ", err);
+  }
   res.send("Data");
 })
 
 // TODO: POST todays time
 app.post('/bedtime', (req, res) => {
   res.send("Bedtime");
-})
-
-// TODO: POST unsync partners
-app.post('/unsync', (req, res) => {
-  res.send("Unsync");
 })
 
 app.listen(3001);
