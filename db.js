3551616bd313c70c94e619810628329c7ae2da09
BaysheffDev <42050015+BaysheffDev@users.noreply.github.com>
2019-11-08 23:10:33 +1030

    added ssl: true in db connection

--- db.js
+++ db.js
@@ -8,9 +8,10 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const PORT = process.env.PORT;
 // const connectionString = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@localhost:${PORT}/gotobedskat`;
-const connectionString: process.env.DATABASE_URL,
+const connectionString: process.env.DATABASE_URL;
 const db = new Pool({
 	connectionString: connectionString,
+	ssl: true,
 })
 
 module.exports = {
