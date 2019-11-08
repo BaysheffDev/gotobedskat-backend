3f3b541ad99b3efe06982eee94848216f1c73f9d
BaysheffDev <42050015+BaysheffDev@users.noreply.github.com>
2019-11-09 00:01:57 +1030

    pushing again

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 // POSTGRES DB CONNECTION
 const {Pool, Client} = require('pg');
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const PORT = process.env.PORT;
+// const DB_USERNAME = process.env.DB_USERNAME;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+// const PORT = process.env.PORT;
 // const connectionString = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@localhost:${PORT}/gotobedskat`;
 const connectionString = process.env.DATABASE_URL;
 const db = new Pool({
